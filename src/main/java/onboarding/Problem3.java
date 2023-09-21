73cea77dffd7e95720ce91610f47bc9c60281d52
davemins <davemins4125@naver.com>
2023-09-21 12:25:13 +0900

    feat : clapCount 메소드 추가 - 매개값에 대해 몇 번 박수를 쳐야하는지 리턴

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,12 +1,30 @@
 /*
 369 게임 손벽을 몇번 쳐야 하는지 return
+숫자를 보고 출력하는 것이 아닌 그 누적된 수를 보고 출력함
+
+3,6,9,13,16,19,23,26,29,30,31,32,33,34,35,36,37,38,39,43,44
  */
 
 package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
+
+        int answer = clapResult(number);
         return answer;
+    }
+
+
+
+    private static int clapCount(int number) {
+        int count = 0;
+        while (number != 0) {
+            int digit = number % 10;
+            if (digit == 3 || digit == 6 || digit == 9) {
+                count += 1;
+            }
+            number /= 10;
+        }
+        return count;
     }
 }
